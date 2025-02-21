66f9a6ff3c3a425c8a58fd395d472c16b88b7e34
Arthur Rosa <arthur_rosa@student.uml.edu>
2025-02-20 23:40:58 -0500

    making changes to java files

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -2,7 +2,7 @@ package com.leszko.calculator;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-// TEST
+// TEST Making changes
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
