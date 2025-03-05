8dfb68bd6dcbe0f11973134e98b688c909c14ba9
Arthur Rosa <arthur_rosa@student.uml.edu>
2025-03-04 23:57:54 -0500

    test

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -2,6 +2,11 @@ package com.leszko.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+/**
+ * The {@code Calculator} class provides basic arithmetic operations.
+ * This class includes a method for summing two integers and utilizes caching 
+ * to improve performance.
+ */
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
 
