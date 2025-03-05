a41ea679adf58caf59905713392a380b5eaa831b
Arthur Rosa <arthur_rosa@student.uml.edu>
2025-03-04 23:48:05 -0500

    test

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -2,10 +2,22 @@ package com.leszko.calculator;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-// TEST
+/**
+ * The {@code Calculator} class provides basic arithmetic operations.
+ * This class includes a method for summing two integers and utilizes caching 
+ * to improve performance.
+ */
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
+	 /**
+     * Computes the sum of two integers. This method is cached to enhance performance
+     * when computing the same inputs multiple times.
+     *
+     * @param a the first integer
+     * @param b the second integer
+     * @return the sum of {@code a} and {@code b}
+     */
 	@Cacheable("sum")
 	public int sum(int a, int b) {
 		return a + b;
