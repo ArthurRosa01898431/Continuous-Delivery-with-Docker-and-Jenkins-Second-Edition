94278eb79a84bd365fd4d86763aaee2c8eb063a7
Arthur Rosa <arthur_rosa@student.uml.edu>
2025-03-18 21:55:53 -0400

    test

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
 
+/*
+test
+*/
      @Test
      public void testSum() {
           assertEquals(5, calculator.sum(2, 3));
